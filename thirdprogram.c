07a1d27d233f0395ab49ecf3be24c323f8aee8b5
ManasShukla618 <manasshukla745@gmail.com>
2026-03-23 12:20:54 +0530

    Add files via upload

---
+++ thirdprogram.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int arr[10];
+int n,i;
+printf("Enter the size of the array");
+scanf("%d",& n);
+for(i=0;i<=arr.length;i++)
+{
+    printf("%d",arr[i]);
+}
+if(arr[i]>arr[0])
+max=arr[i];
+}
+printf("%d",max);
+}
+
+    return 0;
+}
